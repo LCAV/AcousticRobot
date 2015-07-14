a0a09e9272dde240a89836b9f621e879d1b9ae61
Frederike Dümbgen <kiki@tsf-436-wpa-1-229.epfl.ch>
2015-07-14 14:27:31 +0200

    Updated version of read_timed_file_windows.py with improved timing (sleep duration adapted depending on execution time), control of get and set parameters set up but not completed yet (the valid commands need to be confirmed on the robot first), writing of parameters into seperate output files (only printing out so far)

--- read_timed_file_windows.py
+++ read_timed_file_windows.py
@@ -9,23 +9,52 @@ ts = 0.1 #default sampling time [s]
 ''' Get parameters from command line '''
 def get_parameters():
 	inputfile=''
+	outputfile='default.txt'
 	t_sample = ts
 	try: 
-		opts,args = getopt.getopt(sys.argv[1:],"i:t:",["ifile=","t="])
+		opts,args = getopt.getopt(sys.argv[1:],"i:o:t:",["ifile=","ofile=","t="])
 	except getopt.GetoptError:
-		print("error1: syntax is file.py -i <inputfile> -t <sampling time [s]>")
+		print("error1: syntax is file.py -i <inputfile> -o <outputfile> -t <sampling time [s]>")
 		sys.exit()
 	for opt, arg in opts:
 		if opt == '-h':
-			print("error2: syntax is file.py -i <inputfile> -t <sampling time [s]>")
+			print("error2: syntax is file.py -i <inputfile> -o <outputfile> -t <sampling time [s]>")
 			sys.exit()
 		elif opt in ("-i","--ifile"):
 			inputfile = arg
 		elif opt in ("-t","--t"):
-			t_sample = float(arg)
+			try:
+				t_sample = float(arg)
+			except:
+				print('error3: please specify a valid sampling time')
+				sys.exit()
+		elif opt in ("-o","--ofile"):
+			try:
+				with open(arg) as f:
+					outputfile = arg
+			except:
+				print('error4: please choose a valid output file')
+				sys.exit()
 	print('Input file is',inputfile)
+	print('Output file is',outputfile)
 	print('Sampling time is',t_sample)
-	return inputfile,t_sample
+	return inputfile,outputfile,t_sample
+	
+''' Read p and g commands '''
+def read_setandget(command):
+	if len(command) >= 5 : # minimum length of get and set command
+		if (command[1] == ' ') and (command[3] == ' '): # spaces after command and board
+			if valid_boards.count(command[2]) > 0: # board is valid
+				if command[0] == 'g':
+					# get mode
+					print('get mode')
+				elif command[0] == 'p':
+					# set mode
+					print('set mode')
+	return 1			
+
+							
+	
 
 
 ''' Read file and check if it is valid '''
@@ -59,7 +88,7 @@ def read_file(inputfile,ts):
 					sys.exit()
 				else:
 				# Check if all commands are valid
-					if valid_array.count(file_command) > 0:
+					if (valid_array.count(file_command) > 0) or read_setandget(file_command): # command is valid
 						time_array.append(file_time)
 						command_array.append(file_command)
 					else:
@@ -75,23 +104,35 @@ def read_file(inputfile,ts):
 
 
 ''' Send commands '''
-def send_commands(time_array, command_array):
+def send_commands(time_array, command_array,outputf):
 	counter = 0
 	end_time = time_array[-1] # last element
 	real_start = time.time()
-	for i in list(range(int(end_time/ts)+1)):
-		#print(i)
-		#print(time_array[counter]/ts)
-		if (i == (int(time_array[counter]/ts))): # current time is one of the file times
-			print('{0} \t {1:5.4f} \t {2:5.4f}'.format(command_array[counter], time_array[counter], time.time()-real_start))
-			counter += 1
-		time.sleep(ts)
+	last_time = 0 # used for timing real operations
+	with open(outputf,'wb') as f:
+		c=csv.writer(f,delimiter='\t')
+		for i in list(range(int(end_time/ts)+1)):
+			command = command_array[counter]
+			times =  time_array[counter]
+			this_time = time.time()
+			if (i == (int(times/ts))): # current time is one of the file times
+				print('{0:15s} \t {1:5.4f} \t {2:5.4f}'.format(command, times, this_time-real_start))
+				counter += 1
+				if command[0] == 'g':
+					#print('write parameter and time to file')
+					c.writerow([times,command])		
+			time.sleep(ts-(time.time()-this_time))
 
 
 ''' Initialization '''
 valid_array = ['i','f','b','l','r','e','v','u','d','a','o','c','sh','sa','s','pl','pr','g','tu','td','z'] # valid commands
+valid_boards = ['r','l','n','o']
+valid_params_motors = ['NOM_SPEED','KP','KI','Kd','ACC_INC','ACC_DEC','MIN_SPEED','POS_MARG','SPEED_POS']
+valid_params_neck = ['NOM_PWM','NOM_ANGLE']
+valid_params_other = ['PORT','LED_GPIO','PAN_NOM_SPEED','TILT_NOM_SPEED']
+
 ts = 0.1 #default sampling time [s]
-inputfile,ts = get_parameters()
+inputfile,outputfile,ts = get_parameters()
 (times, commands) = read_file(inputfile,ts)
-send_commands(times, commands)
+send_commands(times, commands,outputfile)
 
