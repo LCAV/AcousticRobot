00685e832e434c6fc1f3b454a566d37a6ae58965
Frederike Dümbgen <kiki@tsf-436-wpa-1-229.epfl.ch>
2015-07-15 10:34:24 +0200

    removed old files

--- read_timed_file_windows.py
+++ read_timed_file_windows.py
@@ -6,7 +6,7 @@ import csv, time, sys, getopt
 ''' Get parameters from command line '''
 def get_parameters():
 	inputfile=''
-	outputfile='default.txt'
+	outputfile='./output/default.txt'
 	t_sample = ts
 	try: 
 		opts,args = getopt.getopt(sys.argv[1:],"i:o:t:",["ifile=","ofile=","t="])
@@ -40,7 +40,6 @@ def get_parameters():
 	print('Input file is',inputfile)
 	print('Output file is',outputfile)
 	print('Sampling time is',t_sample)
-	sys.exit()
 	return inputfile,outputfile,t_sample
 	
 ''' Read p and g commands '''
@@ -56,55 +55,49 @@ def read_setandget(command):
 					print('set mode')
 	return 1			
 
-							
-	
-
-
 ''' Read file and check if it is valid '''
 def read_file(inputfile,ts):
 	file_last = 0
 	command_array = []
 	time_array = []
 	counter = 0
-	try:
-		with open(inputfile) as f:
-			c=csv.DictReader(f,delimiter = '\t', fieldnames = ['time','command'],skipinitialspace=True)
-			for line in c :
+	with open(inputfile) as f:
+		c=csv.DictReader(f,delimiter = '\t', fieldnames = ['time','command'],skipinitialspace=True)
+		for line in c :
+			try:
 				file_time = float(line['time'])
-				file_command = line['command']
-				diff_file = file_time - file_last
-				# Check if file starts with i 
-				if (counter == 0) and (file_command != 'i'):
-					while 1:
-						choice = raw_input("warning1: file doesn't start with 'i'. Continue anyways?(yes=y,no=n) ")
-						if choice == 'y':
-							break
-						elif choice =='n':
-							sys.exit()
-				# Check if time increments correctly
-				if diff_file < 0:
-					print('non-incrementing time value after ',time_array)
-					sys.exit()
-				# Check if time is multiple of sampling time
-				elif (round(file_time/ts)%1) > 0: # use round because of rounding errors
-					print('not a multiple of sampling time:', file_time )
-					sys.exit()
-				else:
-				# Check if all commands are valid
-					if (valid_array.count(file_command) > 0) or read_setandget(file_command): # command is valid
-						time_array.append(file_time)
-						command_array.append(file_command)
-					else:
-						print('non-valid command ',file_command)
+			except:
+				print('error4: non-valid time:',line['time'])
+				sys.exit()
+			file_command = line['command']
+			diff_file = file_time - file_last
+			# Check if file starts with i 
+			if (counter == 0) and (file_command != 'i'):
+				while 1:
+					choice = raw_input("warning1: file doesn't start with 'i'. Continue anyways?(yes=y,no=n) ")
+					if choice == 'y':
+						break
+					elif choice =='n':
 						sys.exit()
-				counter += 1
-				file_last = file_time
-			return time_array, command_array
-	except EnvironmentError:
-		print("Please enter a valid filename")
-		return 0 
-		sys.exit()
-
+			# Check if time increments correctly
+			if diff_file < 0:
+				print('error5: non-incrementing time value after ',time_array)
+				sys.exit()
+			# Check if time is multiple of sampling time
+			elif (round(file_time/ts)%1) > 0: # use round because of rounding errors
+				print('error6: not a multiple of sampling time:', file_time )
+				sys.exit()
+			else:
+			# Check if all commands are valid
+				if (valid_array.count(file_command) > 0) or read_setandget(file_command): # command is valid
+					time_array.append(file_time)
+					command_array.append(file_command)
+				else:
+					print('non-valid command ',file_command)
+					sys.exit()
+			counter += 1
+			file_last = file_time
+		return time_array, command_array
 
 ''' Send commands '''
 def send_commands(time_array, command_array,outputf):
@@ -126,6 +119,10 @@ def send_commands(time_array, command_array,outputf):
 					c.writerow([times,command])		
 			time.sleep(ts-(time.time()-this_time))
 
+def default_stop():
+	time.sleep(2)
+	print('robot stopped automatically')
+	
 
 ''' Initialization '''
 valid_array = ['i','f','b','l','r','e','v','u','d','a','o','c','sh','sa','s','pl','pr','g','tu','td','z'] # valid commands
@@ -138,4 +135,4 @@ ts = 0.1 #default sampling time [s]
 inputfile,outputfile,ts = get_parameters()
 (times, commands) = read_file(inputfile,ts)
 send_commands(times, commands,outputfile)
-
+default_stop()
