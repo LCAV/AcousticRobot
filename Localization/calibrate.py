calibrate: fixed get_objectpoints (but still no good results), location: added calculation of individual robot positions using get_objectpoints. perspective: CHANGED MIN_AREA, RADIUS SUCH THAT PROGRAM WORKS FOR TEST ON WEDNESDAY, 2.9.2015

Localization/calibrate.py
@@ -100,7 +100,14 @@ def get_leastsquares(cam,pt,method='hz',r_height='',p_real=''):
     b_part = dict()
     x=err2=err3=0
     if method == 'hz':
-        '''DLT Method of Hartley Zisserman, chapter 12.2 '''
+        '''
+        DLT Method of Hartley Zisserman, chapter 12.2
+        if height specified: least squares algorithm can be applied.
+        if not: use singular value decomposition to find vector corresponding
+        to smallest singular value.
+        '''
+
+        # construct A as in p.312
         for i,c in enumerate(cam):
             f1 = c.Proj[:1]
             f2 = c.Proj[1:2]
@@ -110,7 +117,10 @@ def get_leastsquares(cam,pt,method='hz',r_height='',p_real=''):
             y = p[0,1]
             A_part[i] = np.vstack((x*f3-f1,y*f3-f2))
 
+        # Stack A matrices of each camera together.
         A_aug = np.vstack(A_part.values())
+
+        # r_height specified:
         if type(r_height)!=str:
             a1 = A_aug.T[:1]
             a2 = A_aug.T[1:2]
@@ -121,16 +131,7 @@ def get_leastsquares(cam,pt,method='hz',r_height='',p_real=''):
             x_hat = (A_aug2.T*A_aug2).I*A_aug2.T*b_aug.T
             control = A_aug*np.vstack((x_hat,r_height,1))
             x =  np.vstack((x_hat,r_height))#control,0
-            # Algorithm page 590 if matrix A_aug is singular (not tested!
-            #u,w,v = svd(A_aug)
-            #b_prime = u.T*b_aug
-            #y = np.zeros((v.shape[1],1))
-            #for i in range(v.shape[1]):
-            #    try:
-            #        y[i]=b_aug[0,i]/w[i]
-            #    except:
-            #        y[i]=0
-            #x_hat = v[0]*y
+        # r_height not specified:
         else:
             u,w,v = svd(A_aug)
             x_hat = v[-1]
@@ -376,23 +377,25 @@ class Camera:
         Returns images of reference points (known) and the relative error matrix
         '''
         test_img = self.Proj*p_obj.T
-        test_img = test_img/test_img[2,:]
+        lamda = test_img[2,:]
+        test_img = test_img/lamda
         test_img=test_img[:2,:].T
         #match_matrix=(p_img-test_img)/test_img
         err_img=np.sqrt(np.sum(np.power(test_img[:,:2]-p_img[:,:2],2),axis=1))
-        return test_img,err_img
+        return test_img,err_img,lamda
     def check_objectpoints(self,p_obj,p_img):
         # From least squares
         A =(self.Proj.T*self.Proj).I*self.Proj.T
-        test_obj =A*cam.C.I*p_img.T
-        test_obj2 = self.Proj.I*self.C.I*p_img.T
-        # eliminate last column that seems to screw everything up
-        test_obj3 = A[:,:2]*(cam.C.I*p_img.T)[:2,:]
-        # From algebraic solving
-        test_obj = test_obj/test_obj[3,:]
-        test_obj2 = test_obj2/test_obj2[3,:]
+        x_tilde = self.C.I*p_img.T
+        test_obj =A*x_tilde
 
-        err_obj=np.sqrt(np.sum(np.power(test_obj[:,:3]-p_obj[:,:3],2),axis=1))
+        #test_obj2 = self.Proj.I*self.C.I*p_img.T
+
+        # eliminate last column that seems to screw everything up
+        #test_obj3 = A[:,:2]*(cam.C.I*p_img.T)[:2,:]
+
+        test_obj = test_obj/test_obj[3,:]
+        err_obj=np.sqrt(np.sum(np.power(test_obj.T[:,:3]-p_obj[:,:3],2),axis=1))
         return test_obj, err_obj
     def get_theta(self):
         __,__,__,x,y,z,__ =cv2.decomposeProjectionMatrix(self.Proj)
@@ -416,7 +419,7 @@ class Camera:
                 ref_img = img.ref_img[result].reshape(result.shape[0],2)
                 ref_real = img.ref_real[result].reshape(result.shape[0],3)
                 # Check if the match is new best match
-                ref, err_img = self.check_imagepoints(img.augment(ref_real),ref_img)
+                ref, err_img,__ = self.check_imagepoints(img.augment(ref_real),ref_img)
                 sum_current = err_img.sum()/err_img.shape[0]
                 if sum_current < sum_max:
                     sum_max = sum_current
@@ -466,7 +469,6 @@ class Camera:
         cv.Remap(src, dst, mapx, mapy, cv.CV_INTER_LINEAR + cv.CV_WARP_FILL_OUTLIERS,  cv.ScalarAll(0))
         # cv.Undistort2(src,dst, intrinsics, dist_coeffs)
         return np.array(dst)
-
 
 
 class Image:
@@ -597,7 +599,7 @@ if __name__ == '__main__':
 
         ######################### INDIVIDUAL METHODS #######################
         #------------------------- Check transform ------------------------#
-        img.ref,err_img = cam.check_imagepoints(img.augment(img.ref_real),img.ref_img)
+        img.ref,err_img,__ = cam.check_imagepoints(img.augment(img.ref_real),img.ref_img)
         print("ind: ref point img match [px]",cam.n,"\n",err_img)
         #--------------------------- Get Robot 3D -------------------------#
         img.r,err2,err3 = get_leastsquares([cam],[img.augment(img.r_img)],

Localization/location.py
@@ -48,10 +48,10 @@ if __name__ == '__main__':
 #---------------------------       Initialization       -----------------------#
     cam_dir = 'calib/'
     out_dir,n_pts,fisheye = get_param() # number of reference points
-    n_cameras = [141]
-    #n_cameras = [139,141,145]
-    #p_real = np.matrix([750,790,190])
-    p_real = np.matrix([645,930,180])
+    #n_cameras = [141]
+    n_cameras = [139,141,143]
+    #n_cameras = [139,141]
+    p_real = np.matrix([1822,1516,182])
     C139_real = np.matrix([110,670,1750])
     C141_real = np.matrix([460,40,1320])
     r_height = p_real[0,2] #height of robot in mm
@@ -89,7 +89,7 @@ if __name__ == '__main__':
             #-- get points --#
             col_min = np.array([175,100,0],dtype=np.uint8)
             col_max = np.array([20,250,255],dtype=np.uint8)
-            r = 30
+            r = 10
             t = 50
             img_org,circ_org,pts_img,th_org = persp.imagepoints(img,r,n_pts,t,
                                                                 col_min,col_max)
@@ -97,7 +97,7 @@ if __name__ == '__main__':
             break
 #--------------------------- 3.b Get Object Points      -----------------------#
         #-- get points --#
-        pts_obj, margin,M = persp.objectpoints(6)
+        pts_obj, margin,M = persp.objectpoints(n_pts)
         img_test,pts_obj,size = persp.format_points(pts_obj,margin)
         img_flat,M = persp.geometric_transformationN(img,pts_obj,pts_img,size)
 
@@ -137,12 +137,15 @@ if __name__ == '__main__':
                 img_ref.read_ref(out_dir,"ref_",n_pts)
                 col_min = np.array([150,100,0],dtype=np.uint8)
                 col_max = np.array([20,250,255],dtype=np.uint8)
-                r_rob = 40
-                r_ref = 30
+                #r_rob = 40
+                #r_ref = 30
+                #t = 50
+                #img_red,circ_red,p,th_red = persp.imagepoints_auto(img,r_rob,1,t,
+                #                                              col_min,col_max,
+                #                                              img_ref.ref_img,r_ref)
+                r = 10
                 t = 50
-                img_red,circ_red,p,th_red = persp.imagepoints_auto(img,r_rob,1,t,
-                                                              col_min,col_max,
-                                                              img_ref.ref_img,r_ref)
+                img_red,circ_red,p,th_red = persp.imagepoints(img,r,1,t,col_min,col_max)
                 #-- save resutls --#
                 name='pos_img'+str(n)+'.txt'
                 persp.write_pos(out_dir,name,p)
@@ -167,15 +170,22 @@ if __name__ == '__main__':
                 err_img = cam.ransac_loop(img,flag,repErr_range)
             else:
                 cam.reposition(img.ref_real,img.ref_img,0,flag,ransac)
-                ref, err_img = cam.check_imagepoints(img.augment(img.ref_real),
+                ref, err_img,lamda = cam.check_imagepoints(img.augment(img.ref_real),
                                                      img.ref_img)
-
+                ref_obj, err_obj = cam.check_objectpoints(img.augment(img.ref_real),img.augment(img.ref_img))
             #--- Individual Robot position ---#
             img.r,err2,err3 = calib.get_leastsquares([cam],[img.augment(img.r_img)],
-                                                    'my',r_height,p_real)
+                                                    'hz',r_height,p_real)
+            msg00="fixed [mm]: [{0:8.4f} , {1:8.4f} , {2:8.4f}], error 2D: {3:5.2f} 3D: {4:5.2f}".format(float(img.r[0]),float(img.r[1]),float(img.r[2]),err2,err3)
+            img.r,err2,err3 = calib.get_leastsquares([cam],[img.augment(img.r_img)],
+                                                    'hz','',p_real)
+            msg01="free [mm]: [{0:8.4f} , {1:8.4f} , {2:8.4f}], error 2D: {3:5.2f} 3D: {4:5.2f}".format(float(img.r[0]),float(img.r[1]),float(img.r[2]),err2,err3)
+            print("{0}: {1}".format(n,msg00))
+            print("{0}: {1}".format(n,msg01))
             imgs[i]=img
             cams[i]=cam
             pts[i]=img.augment(img.r_img)
+            errs[i]=err_img
 
         # For all permutations (when more than 1 camera)
         p_lq,err2,err3 = calib.get_leastsquares(cams.values(),pts.values(),
@@ -183,19 +193,25 @@ if __name__ == '__main__':
         p_lq2,err22,err32 = calib.get_leastsquares(cams.values(),pts.values(),
                                                  'hz','',p_real)
 
-
+        errors = np.matrix([round(np.sum(x)/len(x),4) for x in errs.values()])
         # Results visualization and saving
+
         msg1="Fixed height [mm]: [{0:8.4f} , {1:8.4f} , {2:8.4f}], error 2D: {3:5.2f} 3D: {4:5.2f}".format(float(p_lq[0]),float(p_lq[1]),float(p_lq[2]),err2,err3)
         msg2="Free height [mm]:  [{0:8.4f} , {1:8.4f} , {2:8.4f}], error 2D: {3:5.2f} 3D: {4:5.2f}".format(float(p_lq2[0]),float(p_lq2[1]),float(p_lq2[2]),err22,err32)
         msg3="Real position [mm]:[{0:8.4f} , {1:8.4f} , {2:8.4f}]".format(p_real[0,0],p_real[0,1],p_real[0,2])
-
+        msg4="Error reference poitns [px]: {0} ".format(errors)
         print(msg1)
         print(msg2)
         print(msg3)
+        print(msg4)
+
         with open(out_dir+"results.txt",'w') as f:
             f.write(msg1+"\n")
             f.write(msg2+"\n")
             f.write(msg3+"\n")
+            f.write(msg4+"\n")
+
+
 
         choice = raw_input(ROBOT_LOC)
 

Localization/perspective.py
@@ -11,7 +11,7 @@ import marker_calibration as mark
 import time
 import get_image as get
 
-DEBUG = 0
+DEBUG = 1
 USAGE = '''
 usage:
 Option 1:
@@ -211,7 +211,7 @@ def extract_color(img,range_min,range_max):
     ''' Color contours extraction '''
     i=0
     max_area=0
-    min_area = 500
+    min_area = 60
     best_cnt = 1
     cx = cy = 0
 
@@ -407,17 +407,24 @@ def objectpoints(m):
     D[1,2] = D[2,1] = s + marker_diameter
     D[1,3] = D[3,1] = d + marker_diameter
     D[3,2] = D[2,3] = s + marker_diameter
+
+    D[0,1] = D[1,0] = 1.602 + marker_diameter
+    D[0,2] = D[2,0] = 3.202 + marker_diameter
+    D[0,3] = D[3,0] = 2.644 + marker_diameter
+    D[1,2] = D[2,1] = 2.050 + marker_diameter
+    D[1,3] = D[3,1] = 2.325 + marker_diameter
+    D[2,3] = D[3,2] = 1.459 + marker_diameter
     if m>=5:
-        D[0,4] = D[4,0] = 0.61 + marker_diameter
-        D[1,4] = D[4,1] = 0.40 + marker_diameter
-        D[2,4] = D[4,2] = 0.42 + marker_diameter
-        D[3,4] = D[4,3] = 0.62 + marker_diameter
+        D[0,4] = D[4,0] = 1.734 + marker_diameter
+        D[1,4] = D[4,1] = 2.412 + marker_diameter
+        D[2,4] = D[4,2] = 2.713 + marker_diameter
+        D[3,4] = D[4,3] = 1.447 + marker_diameter
     if m>=6:
-        D[0,5] = D[5,0] = 0.93 + marker_diameter
-        D[1,5] = D[5,1] = 1.06 + marker_diameter
-        D[2,5] = D[5,2] = 0.58 + marker_diameter
-        D[3,5] = D[5,3] = 0.28 + marker_diameter
-        D[4,5] = D[5,4] = 0.66 + marker_diameter
+        D[0,5] = D[5,0] = 1.735 + marker_diameter/2
+        D[1,5] = D[5,1] = 1.096 + marker_diameter/2
+        D[2,5] = D[5,2] = 1.489 + marker_diameter/2
+        D[3,5] = D[5,3] = 1.257 + marker_diameter/2
+        D[4,5] = D[5,4] = 1.588 + marker_diameter/2
     # Get postions
     M1 = mark.MarkerSet(m=m,dim=dim,diameter=marker_diameter)
     M1.fromEDM(D**2)
@@ -543,7 +550,7 @@ if __name__ == "__main__":
                 #-------------- Project image to 2D -----------#
                 # Get real positions
                 #TODO only for testing
-                pts_obj2,margin,M = objectpoints(6)
+                pts_obj2,margin,M = objectpoints(m)
                 # position of real points in cm, pt = (x,y)
                 pts_obj_test=np.array([[0,0],[70,0],[70,70],[0,70]],dtype=np.float32)
                 pts_obj_test=np.vstack((pts_obj_test,[50,33],[16,90]))