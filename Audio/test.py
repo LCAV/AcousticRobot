2e3da1434b5ab296075af5a03bc83cff6432572c
duembgen <frederike.duembgen@epfl.ch>
2015-10-14 11:09:01 +0200

    text.py working for MOTU Soundcard but with only one input and output channel.

--- Audio/test.py
+++ Audio/test.py
@@ -1,14 +1,24 @@
+# -*- coding: utf-8 -*-
 """ Play and Record Test """
-
+from __future__ import print_function, division
 import pyaudio
 import wave
 import sys
+import numpy as np
 
+def get_device_index(p):
+    ''' Lets user manually choose the device used for audio input and output.'''
+    for i in range(p.get_device_count()):
+        print("Index ",i,":\n",p.get_device_info_by_index(i))
+
+    index = int(input("\nEnter index of Audio device to be used: \n"))
+    return index
+
+# input information
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS=2
 RATE=44100
-RECORD_SECONDS = 5
 
 if len(sys.argv) < 2:
      print("Plays a wave file.\n\nUsage: %s input.wav output.wav" % sys.argv[0])
@@ -17,34 +27,43 @@ if len(sys.argv) < 2:
 FIN = sys.argv[1]
 FOUT = sys.argv[2]
 
-wf = wave.open(FIN,'rb')
-
+wf_in = wave.open(FIN,'rb')
+# create managing PyAudio instances
 p_out = pyaudio.PyAudio()
 p_in = pyaudio.PyAudio()
 
-stream_out = p_out.open(format=p_out.get_format_from_width(wf.getsampwidth()),
-                        channels=wf.getnchannels(),
-                        rate=wf.getframerate(),
+INDEX = get_device_index(p_in)
+
+# initialize streams
+stream_out = p_out.open(output_device_index=INDEX,
+                        format=p_out.get_format_from_width(wf_in.getsampwidth()),
+                        channels=wf_in.getnchannels(),
+                        rate=wf_in.getframerate(),
                         output=True)
-stream_in = p_in.open(format=FORMAT,
+stream_in = p_in.open(input_device_index=INDEX,
+                      format=FORMAT,
                       channels=CHANNELS,
                       rate=RATE,
                       input=True,
                       frames_per_buffer=CHUNK)
+# initialize frames (to store recorded data in)
 frames = []
+frames_decoded = []
 
 # read data
-data = wf.readframes(CHUNK)
-# play stream_out
+data = wf_in.readframes(CHUNK)
 while data != '':
+    # play
     stream_out.write(data)
-    data = wf.readframes(CHUNK)
+    data = wf_in.readframes(CHUNK)
+    # record
     data_in = stream_in.read(CHUNK)
     frames.append(data_in)
+    frames_decoded.append(np.fromstring(data_in,np.int16))
+frames_decoded=np.matrix(frames_decoded)
+wf_in.close()
 
-wf.close()
-
-# stop stream_out
+# stop streams
 stream_out.stop_stream()
 stream_out.close()
 stream_in.stop_stream()
@@ -53,12 +72,12 @@ stream_in.close()
 # close PyAudio
 p_in.terminate()
 
-# save audio file
-wf = wave.open(FOUT, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p_out.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+# save recorded data in audio file
+wf_out = wave.open(FOUT, 'wb')
+wf_out.setnchannels(CHANNELS)
+wf_out.setsampwidth(p_out.get_sample_size(FORMAT))
+wf_out.setframerate(RATE)
+wf_out.writeframes(b''.join(frames))
+wf_out.close()
 
 p_out.terminate()
